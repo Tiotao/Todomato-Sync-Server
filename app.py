928a853763b407e4e48a73dc85de0cd15e7604d1
Tiotao <evan@tiotao.info>
2014-04-10 11:32:32 +0800

    change lst

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ def init(local_auth):
 
 def update(client, feed_uri, local_tasklist, remote_tasklist, last_sync):
 
-    if len(local_tasklist) == 0 and last_sync is None:
+    if len(local_tasklist) == 0 and last_sync is '':
         local_tasklist = remote_tasklist
         return local_tasklist
-    elif len(remote_tasklist) == 0 and last_sync is None:
+    elif len(remote_tasklist) == 0 and last_sync is '':
         remote_tasklist = create_remote_tasks(client, feed_uri, local_tasklist)
         local_tasklist = remote_tasklist
         return local_tasklist
